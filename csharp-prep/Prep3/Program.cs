06567d04d64c82ab2d74e24618ecdd9946e0ec0e
PurpleCre <pgonese6@gmail.com>
2024-01-15 12:09:35 +0200

    Finished Prep3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,54 @@
 using System;
+using System.Net;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        bool play = true;
+
+        do
+        {
+            int magicNum = randomGenerator.Next(1, 100);
+
+            string usrGuess = "";
+            int guess = 0;
+            int guesses = 0;
+
+            do
+            {
+                Console.Write("Guess the number: ");
+                usrGuess = Console.ReadLine();
+                guess = int.Parse(usrGuess);
+                guesses += 1;
+
+                if (guess < magicNum)
+                {
+                    Console.WriteLine("Guess higher");
+                }
+                else if (guess > magicNum)
+                {
+                    Console.WriteLine("Guess lower");
+                }
+                else if (guess == magicNum)
+                {
+                    Console.WriteLine($"You guesed it in {guesses} attempts!");
+                    Console.WriteLine();
+                    Console.Write("Would you like to play again? Y/N ");
+                    string response = Console.ReadLine();
+
+                    if (response.ToUpper() == "N")
+                    {
+                        play = false;
+                        Console.WriteLine();
+                        Console.WriteLine("Thanks for playing.");
+                    }
+                }
+            } while (!(guess == magicNum));
+
+
+        } while (play);
+
     }
 }
