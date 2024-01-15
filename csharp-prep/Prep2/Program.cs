1445c2fac0dc0ed5ac35b5f58c4b11e3c53a956b
PurpleCre <pgonese6@gmail.com>
2024-01-15 11:13:40 +0200

    Finished prep2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,55 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Enter grade percentage: ");
+        string gpa = Console.ReadLine();
+        int percentage = int.Parse(gpa);
+        string grade = "";
+
+        if (percentage >= 90)
+        {
+            grade = "A";
+        }
+
+        else if (percentage >= 80)
+        {
+            grade = "B";
+        }
+
+        else if (percentage >= 70)
+        {
+            grade = "C";
+        }
+
+        else if (percentage >= 60)
+        {
+            grade = "D";
+        }
+
+        else if (percentage < 60)
+        {
+            grade = "E";
+        }
+
+        Console.WriteLine();
+        Console.WriteLine($"your grade is {grade}");
+        Console.WriteLine();
+
+
+        if (percentage >= 70)
+        {
+            Console.WriteLine("Congratulatins, you passed!!!!!!!!");
+        }
+        else
+        {
+            Console.WriteLine("You didn't meet the expectations, please do try again.");
+        }
+
+        Console.WriteLine();
+
     }
 }
