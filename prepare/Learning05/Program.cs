892d476c3e9b95fe7cebe139637df20002efc235
PurpleCre <pgonese6@gmail.com>
2024-02-02 10:42:27 +0200

    Finished Learnning05

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Square square = new Square("red", 5);
+        Circle circle = new Circle("blue", 4);
+        Rectangle rectangle = new Rectangle("green", 7, 4.5);
+
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(square);
+        shapes.Add(rectangle);
+        shapes.Add(circle);
+
+        foreach(Shape shape in shapes)
+        {
+            Console.WriteLine(shape.GetColor());
+            Console.WriteLine(shape.GetArea());
+            Console.WriteLine();
+        }
     }
 }
