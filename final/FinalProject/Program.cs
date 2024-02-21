f57382f455e7e30c12f9d5e17aac28dd80feb1e9
PurpleCre <pgonese6@gmail.com>
2024-02-21 19:20:44 +0200

    moreextra

--- final/FinalProject/Program.cs
+++ final/FinalProject/Program.cs
@@ -8,19 +8,5 @@ class Program
         Init init = new Init();
 
         init.Run();
-        // Borrower borrower = new Borrower("Scott", "Lang", 37);
-        // Librarian librarian = new Librarian("Luvy", "Acveda", 26);
-
-
-        // Book book = new Book();
-        // book.LendTo(librarian);
-        // Console.WriteLine(book.IsAvailable());
-        // book.LendTo(borrower);
-        // Console.WriteLine(book.IsAvailable());
-
-
-
-        //     CharacterIdea character = new CharacterIdea();
-        //     character.GetStringRepresentation();
     }
 }
